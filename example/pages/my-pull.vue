99daf870d8c4e59bbd9f6dcaa9f54d62b9736313
hz9527 <981379316@qq.com>
2017-04-19 08:55:55 +0800

    init pull

---
+++ example/pages/my-pull.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class='my-pull'>
+		<mypull>
+      <div class="list-con" slot='list'>
+      	<div class="item" v-for='item in list'>{{item}}</div>
+      </div>
+    </mypull>
+	</div>
+</template>
+
+<script>
+import Mypull from './my-pull/index.vue';
+export default {
+  components: {
+    Mypull
+  },
+
+  data() {
+    return {
+      list: []
+    };
+  },
+
+  created() {
+    for (var i = 0; i < 20; i++) {
+      this.list.push(i);
+    };
+  }
+};
+</script>
+
+<style scoped>
+.my-pull{
+  margin-top: 50px;
+	border: 1px solid #f55;
+}
+.item{
+  height:40px;
+  line-height: 40px;
+  text-align: center;
+  border-bottom: 1px solid #e55;
+}
+</style>
---
+++ example/pages/my-pull/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="pull-con" @touchstart='touchStart' @touchmove='touchMove' @touchend='touchEnd'>
+		<slot name='list'></slot>
+	</div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'all'
+    }
+  },
+
+  data() {
+    return {
+      scrollTag: null,
+      touch: null,
+      state: '',
+      catchState: '',
+      translate: 0
+    };
+  },
+
+  methods: {
+    touchStart(e) {
+      console.log(1);
+    },
+    touchMove(e) {
+      console.log(2);
+    },
+    touchEnd(e) {
+      console.log(3);
+    }
+  },
+
+  mounted() {
+    var target = this.$el;
+    console.log(target);
+    var overFlow = '';
+    while (overFlow !== 'auto' && overFlow !== 'scroll') {
+      if (overFlow) {
+        target = target.parentNode;
+      };
+      console.log(target);
+      overFlow = window.getComputedStyle(target).overflow;
+      console.log(overFlow === 'visible');
+    }
+
+  }
+};
+</script>
+ <style scoped>
+
+ </style>
