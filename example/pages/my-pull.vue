49905244dab3649ce5b2a431437843feebcdc3da
hz9527 <981379316@qq.com>
2017-04-19 23:13:37 +0800

    loadinglist

--- example/pages/my-pull.vue
+++ example/pages/my-pull.vue
@@ -1,9 +1,12 @@
 <template>
 	<div class='my-pull'>
-		<mypull>
+		<mypull :loading-show='loading' @loading='load'>
       <div class="list-con" slot='list'>
       	<div class="item" v-for='item in list'>{{item}}</div>
       </div>
+			<div class="load" slot='bottomLoad' style='height:30px'>
+				...
+			</div>
     </mypull>
 	</div>
 </template>
@@ -17,8 +20,31 @@ export default {
 
   data() {
     return {
-      list: []
+      list: [],
+      loading: false
     };
+  },
+
+  methods: {
+    load(v) {
+      var that = this;
+      this.loading = true;
+      setTimeout(function() {
+        var index;
+        if (v === 'pullTop') {
+          index = that.list[0];
+          for (var i = 0; i < 10; i++) {
+            that.list.unshift(index - i);
+          }
+        } else {
+          index = that.list[that.list.length - 1];
+          for (var j = 0; j < 10; j++) {
+            that.list.push(index + j);
+          }
+        };
+        that.loading = false;
+      }, 2000);
+    }
   },
 
   created() {
@@ -33,6 +59,8 @@ export default {
 .my-pull{
   margin-top: 50px;
 	border: 1px solid #f55;
+	height: 400px;
+  overflow: auto;
 }
 .item{
   height:40px;
--- example/pages/my-pull/index.vue
+++ example/pages/my-pull/index.vue
@@ -1,6 +1,18 @@
 <template>
-	<div class="pull-con" @touchstart='touchStart' @touchmove='touchMove' @touchend='touchEnd'>
+	<div class="pull-con" @touchstart='touchStart' @touchmove='touchMove' @touchend='touchEnd' :style='{"transform": "translate(0," + translate + "px )"}'>
+		<div class="pull" v-show="state == 'pullTop'">
+      {{loadState ? '松开刷新' : '继续下拉刷新'}}
+    </div>
+		<slot name='topLoad'>
+			<div class="loading" v-show="loadingShow && catchState == 'pullTop'">刷新中...</div>
+    </slot>
 		<slot name='list'></slot>
+		<div class="pull" v-show="state == 'pullDown'">{{loadState ? '松开刷新' : '继续下拉刷新'}}</div>
+		<div v-show="loadingShow && catchState == 'pullDown'" ref='pullDown'>
+			<slot name='bottomLoad'>
+				<div class="loading" style='height:30px'>刷新中...</div>
+	    </slot>
+		</div>
 	</div>
 </template>
 
@@ -10,44 +22,110 @@ export default {
     type: {
       type: String,
       default: 'all'
+    },
+    loadingShow: {
+      type: Boolean,
+      default: false
+    },
+    loadHeight: {
+      type: Number,
+      default: 40
+    },
+    moveTime: {
+      type: Number,
+      default: 300
+    }
+  },
+
+  watch: {
+    loadingShow(v) {
+      if (!v) {
+        this.catchState = '';
+        this.bottomHeight = 0;
+      }
     }
   },
 
   data() {
     return {
       scrollTag: null,
+      maxScroll: 0,
       touch: null,
       state: '',
       catchState: '',
-      translate: 0
+      loadState: false,
+      translate: 0,
+      bottomHeight: 0,
+      startTime: 0
     };
   },
 
   methods: {
     touchStart(e) {
-      console.log(1);
+      if (!this.catchState && e.touches.length === 1) {
+        this.touch = e.touches[0];
+        this.startTime = Date.now();
+      };
     },
     touchMove(e) {
-      console.log(2);
+      if (this.state) {
+        e.preventDefault();
+        var height = e.touches[0].clientY - this.touch.clientY;
+        var coeff = Math.abs(height) > 5 ? 2 / Math.log(Math.abs(height)) : 1;
+        this.translate = height * coeff;
+        if (Math.abs(this.translate) > this.loadHeight) {
+          this.loadState = true;
+        }
+      } else if (this.touch && ((this.scrollTag.scrollTop <= 0 && this.type !== 'bottom') ||
+        (this.type !== 'top' && this.scrollTag.scrollTop >= this.maxScroll))) {
+        var moveX = e.touches[0].clientX - this.touch.clientX;
+        var moveY = e.touches[0].clientY - this.touch.clientY;
+        if (Math.abs(moveX) < Math.abs(moveY / 2)) {
+          if (moveY > 0 && this.scrollTag.scrollTop <= 0) {
+            this.state = 'pullTop';
+          } else if (moveY < 0 && this.scrollTag.scrollTop >= this.maxScroll) {
+            this.state = 'pullDown';
+          }
+        }
+      };
+
     },
     touchEnd(e) {
-      console.log(3);
+      // console.log(3);
+      if (this.loadState && Date.now() - this.startTime >= this.moveTime) {
+        // emit event
+        console.log(2);
+        this.catchState = this.state;
+        this.$emit('loading', this.state);
+        if (this.state === 'pullDown') {
+          var that = this;
+          setTimeout(function() {
+            that.bottomHeight = that.$refs.pullDown.offsetHeight;
+            that.scrollTag.scrollTop += that.bottomHeight;
+          }, 0);
+        }
+      };
+      if (this.state) {
+        this.touch = null;
+        this.state = '';
+        this.loadState = false;
+        this.translate = 0;
+      };
+
     }
   },
 
   mounted() {
     var target = this.$el;
-    console.log(target);
     var overFlow = '';
-    while (overFlow !== 'auto' && overFlow !== 'scroll') {
+    while (!(overFlow === 'auto' || overFlow === 'scroll' || target.nodeName === 'BODY')) {
       if (overFlow) {
         target = target.parentNode;
       };
-      console.log(target);
       overFlow = window.getComputedStyle(target).overflow;
-      console.log(overFlow === 'visible');
     }
-
+    this.maxScroll = target.scrollHeight - target.offsetHeight;
+    this.scrollTag = target;
   }
 };
 </script>
